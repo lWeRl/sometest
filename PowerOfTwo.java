9d1ded0530b813e1416e2f595ed72d5a5bae0814
lWeRl <werasdas@gmail.com>
2016-10-15 14:36:42 +0300

    Power of Two

---
+++ PowerOfTwo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * Created by lWeRl on 15.10.2016.
+ */
+public class PowerOfTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int n = sc.nextInt();
+            if (n == 0) break;
+            System.out.println(((n & (n - 1))));
+        }
+    }
+}
